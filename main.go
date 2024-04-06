08ab1d6eda198c634d85ebf6e9fd851ae1a98ae5
eytan <rparse2015@gmail.com>
2024-04-06 13:16:08 +0300

    azure installation

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,5 +16,12 @@ func main() {
 		})
 	})
 
-	router.Run("127.0.0.1:8080")
+	// Azure App Service sets the port as an Environment Variable
+	// This can be random, so needs to be loaded at startup
+	port := os.Getenv("HTTP_PLATFORM_PORT")
+
+	// default back to 8080 for local dev
+	if port == "" {
+		port = "8080"
+	}
 }
